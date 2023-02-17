6c81ff7d2782e27eef31780f07c9c479de1e0934
Yesica <Yesica@Pc-Yesica>
2023-02-17 09:37:29 +0100

    Punto 5 del control

---
+++ src/CVSMacho/Clase1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package CVSMacho;
+
+/**
+ *
+ * @author Yesica
+ */
+public class Clase1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Yesica Macho Rey");
+        System.out.println("hoy es 17/2/2023");
+    }
+    
+}
