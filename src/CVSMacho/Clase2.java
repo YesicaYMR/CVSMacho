2df1b5554e8e0014df08dbfc5a068d772b133380
Yesica <Yesica@Pc-Yesica>
2023-02-17 09:49:37 +0100

    Punto 7 del control

---
+++ src/CVSMacho/Clase2.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CVSMacho;
+
+/**
+ *
+ * @author Yesica
+ */
+public class Clase2 {
+     System.out.println("Estoy en la rama producción");
+     System.out.println("hoy es 17/2/2023");
+}
